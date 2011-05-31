72e4354ebd428db8d18ca868ba94ced1e6831969
Fabien Le Mentec <texane@gmail.com>
2011-05-31 14:23:47 -0500

    [update] waitcord_fsm

--- src/waitcord_fsm.c
+++ src/waitcord_fsm.c
@@ -7,21 +7,31 @@
 extern igreboard_dev_t igreboard_device;
 
 
+static unsigned int prev_msecs = (unsigned int)-1;
 static unsigned int is_removed = 0;
 
 
 static unsigned int cord_fsm_isdone(void* data)
 {
-  return is_removed == 1;
+  return is_removed;
 }
 
 
 static void cord_fsm_next(void* data)
 {
-#if 0 /* TODO */
-  if (cord_is_removed(&is_removed))
-#endif
+  unsigned int msecs;
+  unsigned int is_pushed;
+
+  if (prev_msecs == -1) prev_msecs = swatch_get_msecs();
+
+  msecs = swatch_get_msecs();
+  if ((msecs - prev_msecs) < 100) return ;
+  prev_msecs = msecs;
+  igreboard_get_cord_switch(&igreboard_device, &is_pushed);
+
+  if (is_pushed == 0)
   {
+    is_removed = 1;
     swatch_start_game();
   }
 }
