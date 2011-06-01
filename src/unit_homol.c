c9f5eee9c37f96877d32f404236a61f9c8f30837
Fabien Le Mentec <texane@gmail.com>
2011-06-01 12:56:08 -0500

    [update] unit_homol

--- src/unit_homol.c
+++ src/unit_homol.c
@@ -6,6 +6,7 @@
 #include "igreboard.h"
 #include "swatch.h"
 #include "sonar.h"
+#include "sharp.h"
 #include "lcd.h"
 
 
@@ -16,6 +17,14 @@ extern aversive_dev_t aversive_device;
 /* globals */
 
 static unsigned int is_red;
+
+#define DONE_REASON_GAMEOVER 0
+#define DONE_REASON_SONAR 1
+#define DONE_REASON_SHARP 2
+#define DONE_REASON_SWITCH 3
+#define DONE_REASON_TRAJ 4
+#define DONE_REASON_POS 5
+static unsigned int done_reason;
 
 
 /* aversive */
@@ -83,20 +92,127 @@ static void goto_first_line(void)
 
 static void turn(void)
 {
-#if 0
-  const int16_t a = is_red ? ;
-
-  if (is_red) a = 600;
-  else x = 3000 - 600;
-
+  const int16_t a = is_red ? -90 : 90;
+  aversive_turn(&aversive_device, a);
   wait_done();
-#endif
 }
 
 
+static inline unsigned int min(unsigned int a, unsigned int b)
+{
+  return a < b ? a : b;
+}
+
 static void move_until(void)
 {
-  /* (sharp || switch || done || gameover || avoid) */
+  /* (gameover || pos || sonar || sharp || switch || traj) */
+
+  int is_done;
+  unsigned int fl, fr;
+  unsigned int is_pushed;
+  int16_t a, x, y;
+
+  aversive_move_forward(&aversive_device, 400);
+
+  while (1)
+  {
+    wait_abit();
+
+    /* gameover */
+    if (swatch_is_game_over())
+    {
+      done_reason = DONE_REASON_GAMEOVER;
+      break ;
+    }
+
+    /* position */
+    aversive_get_pos(&aversive_device, &a, &x, &y);
+    if (y <= 200)
+    {
+      done_reason = DONE_REASON_POS;
+      break ;
+    }
+
+    /* sonar */
+    if (sonar_is_detected())
+    {
+      done_reason = DONE_REASON_SONAR;
+      return ;
+    }
+
+    /* sharp */
+    fl = sharp_read_fl();
+    fr = sharp_read_fr();
+#define PAWN_DIST 200
+    if (min(fl, fr) <= PAWN_DIST)
+    {
+      done_reason = DONE_REASON_SHARP;
+      return ;
+    }
+
+    /* switch */
+    igreboard_get_gripper_switch(&igreboard_device, &is_pushed);
+    if (is_pushed == 1)
+    {
+      done_reason = DONE_REASON_SWITCH;
+      return ;
+    }
+
+    /* traj */
+    aversive_is_traj_done(&aversive_device, &is_done);
+    if (is_done)
+    {
+      done_reason = DONE_REASON_TRAJ;
+      return ;
+    }
+  }
+
+  if (done_reason != DONE_REASON_TRAJ)
+    aversive_stop(&aversive_device);
+}
+
+
+static unsigned int handle_done(void)
+{
+  unsigned int can_redo = 0;
+
+  switch (done_reason)
+  {
+  default:
+  case DONE_REASON_POS:
+  case DONE_REASON_GAMEOVER:
+    {
+      break ;
+    }
+
+  case DONE_REASON_SONAR:
+    {
+      can_redo = 1;
+      break ;
+    }
+
+  case DONE_REASON_SHARP:
+  case_done_reason_sharp:
+    {
+      can_redo = 1;
+      break ;
+    }
+
+  case DONE_REASON_SWITCH:
+    {
+      aversive_move_forward(&aversive_device, -100);
+      goto case_done_reason_sharp;
+      break ;
+    }
+
+  case DONE_REASON_TRAJ:
+    {
+      can_redo = 1;
+      break ;
+    }
+  }
+
+  return can_redo;
 }
 
 
@@ -104,12 +220,19 @@ static void move_until(void)
 
 void unit_homol(void)
 {
+  unsigned int can_redo;
+
   initialize();
   first_pos();
   wait_cord();
+  swatch_start_game();
   goto_first_line();
   turn();
+
+ redo_move:
   move_until();
+  can_redo = handle_done();
+  if (can_redo) goto redo_move;
 
   while (1) asm("nop");
 }
