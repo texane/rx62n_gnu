199e39876d1b3eb1fd69e8fc6dec752fe23b4ee6
Fabien Le Mentec <texane@gmail.com>
2011-06-03 17:22:47 -0500

    [add] unit_bonus

---
+++ src/unit_bonus.c
@@ -0,0 +1,312 @@
+#include "config.h"
+
+#include <stdint.h>
+#include "tile.h"
+#include "fsm.h"
+#include "swatch.h"
+#include "aversive.h"
+#include "igreboard.h"
+#include "swatch.h"
+#include "sonar.h"
+#include "sharp.h"
+#include "lcd.h"
+
+
+extern igreboard_dev_t igreboard_device;
+extern aversive_dev_t aversive_device;
+
+
+#define DONE_REASON_GAMEOVER 0
+#define DONE_REASON_SONAR 1
+#define DONE_REASON_SHARP 2
+#define DONE_REASON_SWITCH 3
+#define DONE_REASON_TRAJ 4
+#define DONE_REASON_POS 5
+
+static unsigned int is_red;
+
+
+static inline unsigned int can_restart(unsigned int reason)
+{
+  if ((reason == DONE_REASON_GAMEOVER) || (reason == DONE_REASON_TRAJ))
+    return 0;
+  return 1;
+}
+
+
+static void wait_abit(void)
+{
+  const unsigned int msecs = swatch_get_msecs();
+  while ((swatch_get_msecs() - msecs) < 100) ;
+}
+
+static int wait_done(void)
+{
+  aversive_dev_t* const dev = &aversive_device;
+  int is_done;
+  for (is_done = 0; is_done == 0; )
+  {
+    wait_abit();
+    aversive_is_traj_done(dev, &is_done);
+  }
+
+  return 0;
+}
+
+
+static void initialize(void)
+{
+  /* initialize globals */
+  igreboard_get_color_switch(&igreboard_device, &is_red);
+
+#if CONFIG_ENABLE_SONAR
+  sonar_finalize();
+#endif
+}
+
+
+static void finalize(void)
+{
+  aversive_stop(&aversive_device);
+  aversive_set_asserv(&aversive_device, 0);
+  aversive_set_power(&aversive_device, 0);
+#if CONFIG_ENABLE_SONAR
+  igreboard_disable_sonar(&igreboard_device);
+#endif
+}
+
+
+static inline int16_t clamp_x(int16_t x)
+{
+  if (x < 0) x = 0;
+  else if (x > 3000) x = 3000;
+  return x;
+}
+
+static inline int16_t clamp_y(int16_t y)
+{
+  if (y < 0) y = 0;
+  else if (y > 2100) y = 2100;
+  return y;
+}
+
+static inline int16_t clamp_a(int16_t a)
+{
+  int16_t mod = a % 360;
+  if (mod < 0) mod = 360 + a;
+  return mod;
+}
+
+static void orient_north(void)
+{
+  int16_t a, x, y, beta;
+  aversive_get_pos(&aversive_device, &a, &x, &y);
+
+  a = clamp_a(a);
+
+  if (a < 90) beta = 90 - a;
+  else if (a < 270) beta = -(a - 90);
+  else beta = 90 + 360 - a;
+
+  aversive_turn(&aversive_device, beta);
+  wait_done();
+}
+
+
+static void orient_south(void)
+{
+  int16_t a, x, y, beta;
+  aversive_get_pos(&aversive_device, &a, &x, &y);
+
+  a = clamp_a(a);
+
+  if (a > 270) beta = 270 - a;
+  else if (a < 90) beta = -(90 + a);
+  else beta = 270 - a;
+
+  aversive_turn(&aversive_device, beta);
+  wait_done();
+}
+
+
+static void first_pos(void)
+{
+  int16_t posa, posx, posy;
+  if (is_red) aversive_set_pos(&aversive_device, 0, 50 + 97, 0);
+  else aversive_set_pos(&aversive_device, 180, 3000 - (50 + 97), 0);
+  aversive_get_pos(&aversive_device, &posa, &posx, &posy);
+  aversive_set_pos(&aversive_device, posa, posx, 410);
+}
+
+
+/* helper */
+
+static inline unsigned int min(unsigned int a, unsigned int b)
+{
+  return a < b ? a : b;
+}
+
+static unsigned int wait_something(unsigned int do_pawn, unsigned int is_north)
+{
+  /* return 1 to restart the move */
+
+  unsigned int fl, fr;
+  unsigned int is_pushed;
+  int is_done;
+  unsigned int reason = DONE_REASON_TRAJ;
+  int16_t a, x, y;
+
+  while (1)
+  {
+    wait_abit();
+
+    /* gameover */
+    if (swatch_is_game_over())
+    {
+      finalize();
+      while (1) ;
+      break ;
+    }
+
+#if CONFIG_ENABLE_SONAR
+    /* sonar */
+    sonar_schedule();
+    if (sonar_is_detected())
+    {
+      aversive_stop(&aversive_device);
+      reason = DONE_REASON_SONAR;
+      break ;
+    }
+#endif /* CONFIG_ENABLE_SONAR */
+
+    /* pawn */
+    if (do_pawn)
+    {
+      /* switch */
+      igreboard_get_gripper_switch(&igreboard_device, &is_pushed);
+      if (is_pushed == 1)
+      {
+	aversive_stop(&aversive_device);
+	reason = DONE_REASON_SWITCH;
+	break ;
+      }
+
+      fl = sharp_read_fl();
+      fr = sharp_read_fr();
+#define PAWN_DIST 170
+      if (min(fl, fr) <= PAWN_DIST)
+      {
+	aversive_stop(&aversive_device);
+	reason = DONE_REASON_SHARP;
+	break ;
+      }
+    }
+
+    /* position */
+    aversive_get_pos(&aversive_device, &a, &x, &y);
+    if (is_north && (y >= 1600))
+    {
+      aversive_stop(&aversive_device);
+      reason = DONE_REASON_POS;
+      break ;
+    }
+    else if (y <= 350)
+    {
+      aversive_stop(&aversive_device);
+      reason = DONE_REASON_POS;
+      break ;
+    }
+
+    /* traj */
+    aversive_is_traj_done(&aversive_device, &is_done);
+    if (is_done)
+    {
+      reason = DONE_REASON_TRAJ;
+      break ;
+    }
+  }
+
+  return reason;
+}
+
+/* sub */
+
+static int move_until_pawn(void)
+{
+  /* return 0 if pawn, -1  otherwise */
+  /* assume starting at middle of second case, south */
+
+  unsigned int reason;
+
+ restart_move:
+  aversive_move_forward(&aversive_device, 2000);
+  reason = wait_something(1, 1);
+
+  if ((reason == DONE_REASON_SHARP) || (reason = DONE_REASON_SWITCH))
+    return 0;
+
+  if (can_restart(reason)) goto restart_move;
+  return -1;
+}
+
+
+static void move_to_bonus(void)
+{
+  unsigned int reason;
+  int16_t x, y;
+
+  if (is_red)
+    x = 450 + 350 * 2 + 350 / 2;
+  else
+    x = 3000 - (450 + 350 * 2 + 350 / 2);
+  y = 350 / 2;
+
+ restart_move:
+  aversive_goto_forward_xy_abs(&aversive_device, x, y);
+  reason = wait_something(0, 0);
+  if (can_restart(reason)) goto restart_move;
+}
+
+static void center_table(void)
+{
+  int16_t a, x, y;
+  aversive_get_pos(&aversive_device, &a, &x, &y);
+  aversive_goto_forward_xy_abs(&aversive_device, 3000 / 2, y);
+  wait_something(0, 0);
+}
+
+static void take_pawn(void)
+{
+  fsm_t fsm;
+  unsigned int is_pushed;
+
+  igreboard_get_gripper_switch(&igreboard_device, &is_pushed);
+  if (is_pushed == 1)
+  {
+    igreboard_close_gripper(&igreboard_device);
+    return ;
+  }
+
+  takepawn_fsm_initialize(&fsm);
+  fsm_execute_one(&fsm);
+}
+
+void unit_bonus(void)
+{
+  initialize();
+  first_pos();
+  orient_north();
+
+  if (move_until_pawn() == 0)
+  {
+    take_pawn();
+    center_table();
+    orient_south();
+    move_to_bonus();
+    igreboard_open_gripper(&igreboard_device);
+  }
+
+  finalize();
+
+  while (1) ;
+}
